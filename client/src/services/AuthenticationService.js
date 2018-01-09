482f8fe0b0acd42c93483f0505e74c1cba278930
Dani Vijay <danimvijay@gmail.com>
2018-01-09 19:41:57 +0530

    set up registration

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,13 +1,7 @@
-import Api from '@services/Api'
+import Api from '@/services/Api'
 
 export default {
   register (credentials) {
     return Api().post('register', credentials)
   }
 }
-
-// Sample Req
-// AuthenticationService.register({
-//   email: 'test@test.com',
-//   password: 'testpass'
-// })
