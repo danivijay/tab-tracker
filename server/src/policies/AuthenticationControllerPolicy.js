e71145072f9b5f45142dd881e0d55dcf6825fc8f
Dani Vijay <danimvijay@gmail.com>
2018-01-10 21:43:43 +0530

    server side data validation for register with joi

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,41 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+
+    const {error, value} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'Invalid Email',
+            info: value
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: `Password must be <br>
+            1. Valid charactors : lowercase, uppercase, numeric <br>
+            2. Greater than 8 characters <br>
+            3. Less than 32 characters`,
+            info: value
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information',
+            info: value
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
