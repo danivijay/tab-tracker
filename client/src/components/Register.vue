6fb34f86c5075302c613257873e1014747d0a351
Dani Vijay <danimvijay@gmail.com>
2018-01-11 22:51:06 +0530

    add vuex, add states token, user, loggedin, hide login and register once logged in

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -8,22 +8,27 @@
           </v-toolbar-title>
           </v-toolbar>
           <div class="pl-4 pr-4 pt-2 pb-2">
-            <v-text-field
-              type="email"
-              label="Email"
-              v-model="email"
-            ></v-text-field>
-            <v-text-field
-              label="Password"
-              type="password"
-              v-model="password"
-            ></v-text-field>
-            <div class="error" v-html="error"></div>
-            <v-btn
-              dark
-              class="cyan"
-              @click="register"
-              >Register</v-btn>
+            <form
+            name="tab-tracket-form"
+            autocomplete="off">
+              <v-text-field
+                type="email"
+                label="Email"
+                v-model="email"
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                type="password"
+                v-model="password"
+                autocomplete="new-password"
+              ></v-text-field>
+              <div class="error" v-html="error"></div>
+              <v-btn
+                dark
+                class="cyan"
+                @click="register"
+                >Register</v-btn>
+            </form>
           </div>
         </div>
     </v-flex>
@@ -44,10 +49,12 @@ export default {
     async register () {
       this.error = ''
       try {
-        await AuthenticationService.register({
+        const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
