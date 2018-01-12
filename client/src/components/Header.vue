59b4a32beb7643d54f9db6e04532a2fee0dbce10
Dani Vijay <danimvijay@gmail.com>
2018-01-12 12:23:43 +0530

    add logout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -25,6 +25,13 @@
       @click="navigateTo({name: 'register'})">
         Sign Up
       </v-btn>
+      <v-btn
+      v-if="$store.state.isUserLoggedIn"
+      flat
+      dark
+      @click="logout">
+        Log Out
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -34,6 +41,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
